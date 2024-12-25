4d67be6e9241836c3fbbc168928797c8d37bd938
Akanksha Singh <54772819+Flamingo09@users.noreply.github.com>
2024-12-25 15:44:16 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,3 +2,15 @@ provider "google" {
   project = "terraform-cloud-445206"
   region  = "us-central1"
 }
+resource "google_storage_bucket" "test_bucket_good" {
+  name                        = "test-bucket-good"
+  location                    = "EU"
+  storage_class               = "STANDARD"
+  uniform_bucket_level_access = true
+}
+resource "google_storage_bucket" "test_bucket_bad" {
+  name                        = "test-bucket-bad"
+  location                    = "EU"
+  storage_class               = "STANDARD"
+  uniform_bucket_level_access = false
+}
