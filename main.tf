25e14c8bacfde26dbd985d0eb956272498043100
Akanksha Singh <54772819+Flamingo09@users.noreply.github.com>
2024-12-26 15:10:32 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -8,7 +8,7 @@ resource "google_storage_bucket" "test_bucket_good_3" {
   storage_class               = "STANDARD"
   uniform_bucket_level_access = true
 }
-resource "google_storage_bucket" "test_bucket_bad_2" {
+resource "google_storage_bucket" "test_bucket_bad__3" {
   name                        = "test-bucket-bad"
   location                    = "EU"
   storage_class               = "STANDARD"
